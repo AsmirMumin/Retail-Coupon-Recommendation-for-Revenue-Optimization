03dcdb2fd64060207930bee4547898595203c25a
Christopher Gerling <58044645+christopher3996@users.noreply.github.com>
2021-03-20 17:30:04 +0100

    add comments

--- final_assignment/module_lags.py
+++ final_assignment/module_lags.py
@@ -1,11 +1,25 @@
+"""
+The purpose of this module is to:
+* calculate laged features such as
+** lags (= number of weeks since last purchase)
+** temporal distribution of purchases
+** average number of weeks between two purchases
+"""
+
 import numpy as np
 import pandas as pd
 
 class LagCalculator:
+    """
+    This class provides the functionalities for creating lagged features
+    """
     def __init__(self, input_dataframe):
         self.tmp_df_for_lags = input_dataframe
 
     def calculate_lags(self):
+    """
+    returns: lags between two purchases (= number of weeks since last purchase)
+    """
         self.tmp_df_for_lags["lag_weeks_of_product_per_customer"] = -1
 
         current_product = -1
@@ -54,6 +68,9 @@ class LagCalculator:
         return self.tmp_df_for_lags
 
     def calculate_purchase_temporal_distribution(self, lags):
+        """
+        returns: temporal distribution of purchases
+        """
         self.purchase_temporal_distribution = (
             lags[(lags["week"] < 89) & (lags["product_bought"] == 1)][
                 ["shopper", "product", "week"]
@@ -72,6 +89,9 @@ class LagCalculator:
         return self.purchase_temporal_distribution
 
     def calculate_avg_no_weeks_between_two_purchases(self, lags):
+        """
+        returns: average number of weeks between two purchases
+        """
         self.avg_no_weeks_between_two_purchases = (
             lags[(lags["week"] < 89) & (lags["product_bought"] == 1)][
                 ["shopper", "product", "lag_weeks_of_product_per_customer"]
