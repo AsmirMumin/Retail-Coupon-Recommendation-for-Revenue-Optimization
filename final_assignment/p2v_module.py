55112b04b38d013a48f7ea8ded64fa6361d88a5f
Christopher Gerling <58044645+christopher3996@users.noreply.github.com>
2021-03-20 17:38:58 +0100

    add comments

--- final_assignment/p2v_module.py
+++ final_assignment/p2v_module.py
@@ -1,3 +1,8 @@
+"""
+The purpose of this module is to:
+* train a product2vec model based on gensim:Word2Vec
+"""
+
 import gensim
 from gensim.models import Word2Vec
 from gensim.models.callbacks import CallbackAny2Vec
@@ -10,13 +15,22 @@ class p2v:
         self.product_list = input_baskets
         
     def create_product_list(self):
+        """
+        generate list of all purchased products
+        """
         self.product_list = list(self.product_list)
         self.product_list = [[str(i) for i in line] for line in self.product_list]
 
     def head(self, n=10):
+        """
+        print method
+        """
         print(self.product_list[0:n])
 
     def train_p2v(self, vec_dim=30, epochs=100):
+        """
+        train gensim model
+        """
         epoch_logger = EpochLogger()
         self.vec_dim = vec_dim
         self.p2v_model = Word2Vec(
@@ -30,6 +44,9 @@ class p2v:
         )
 
     def get_insights(self, product_id):
+        """
+        print method of insights
+        """
         print(f"{self.vec_dim}-dimensional vector for {product_id}:")
         print(self.p2v_model.wv[str(product_id)])
         print(f"\nMost similar products for {product_id}:")
@@ -37,7 +54,9 @@ class p2v:
 
 
 class EpochLogger(CallbackAny2Vec):
-    """Print progress of P2V training to console"""
+    """
+    Print progress of P2V training to console
+    """
 
     def __init__(self):
         self.epoch = 0
